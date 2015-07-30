a18ae663f3e668ea1d16ea6877e72e41366f0ef1
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-31 04:23:20 +0900

    Rewrite getters and setters

--- src/pendant/getter.cr
+++ src/pendant/getter.cr
@@ -1,96 +1,67 @@
 module Pendant::Getter
-  def []?(key)
-  end
-
-  def [](key)
-  end
-
-  def keys
-  end
-
-  # :nodoc:
   macro define_pendant_getters
-    # :nodoc:
-    module {{ "PendantGetter".id }}%mod
-      macro included
-        def []?(key)
-          {% for m in @type.methods %}
-            {% if m.args.length == 0 && m.name.stringify != "keys" && !m.name.stringify.starts_with?("__") %}
-              if key == {{ m.name.stringify }} || key == :{{ m.name.stringify }}
-                return self.{{ m.name }}
-              end
-            {% end %}
-          {% end %}
-          {% if @type.superclass && @type.superclass.methods.any?{|m| m.name.stringify == "[]?" && m.args.length == 1} %}
-            super
-          {% else %}
-            nil
-          {% end %}
-        end
-
-        def [](key)
-          {% for m in @type.methods %}
-            {% if m.args.length == 0 && m.name.stringify != "keys" && !m.name.stringify.starts_with?("__") %}
-              if key == {{ m.name.stringify }} || key == :{{ m.name }}
-                return self.{{ m.name }}
-              end
-            {% end %}
-          {% end %}
-          {% if @type.superclass && @type.superclass.methods.any?{|m| m.name.stringify == "[]?" && m.args.length == 1} %}
-            super
-          {% else %}
-            # NOTE: `MissingKey` is deprected.  If next version Crystal is released, it renames to `KeyError`.
-            raise MissingKey.new("Missing getter value: #{key.inspect}")
-          {% end %}
-        end
-
-        # :nodoc:
-        def __pendant_getter_keys
-          m = {{ @type.methods.select do |m|
-            m.args.length == 0 && m.name.stringify != "keys" && !m.name.stringify.starts_with?("__")
-          end.map{|m| m.name.stringify}.uniq }} of String
-          {% if @type.superclass && @type.superclass.methods.any?{|m| m.name.stringify == "keys" && m.args.length == 0} %}
-            m.concat(super).uniq
-          {% else %}
-            m
-          {% end %}
-        end
-
-        @@%keys = nil
-        {% if @type.methods.any?{|m|m.name.stringify == "__pendant_setter_keys"} %}
-          def keys
-            return @@%keys.not_nil! if @@%keys
-            @@%keys = self.__pendant_getter_keys.concat(self.__pendant_setter_keys).uniq
-            @@%keys.not_nil!
-          end
-        {% else %}
-          def keys
-            return @@%keys.not_nil! if @@%keys
-            @@%keys = self.__pendant_getter_keys
-            @@%keys.not_nil!
+    def []?(key)
+      {% for m in @type.methods %}
+        {% if m.args.length == 0 && m.name.stringify != "keys" %}
+          if key == {{ m.name.stringify }} || key == :{{ m.name.stringify }}
+            return self.{{ m.name }}
           end
         {% end %}
+      {% end %}
+
+      {% if @type.superclass && @type.superclass.methods.any?{|m|m.name.stringify == "[]?" && m.args.length == 1} %}
+        super
+      {% else %}
+        nil
+      {% end %}
+    end
+
+    def [](key)
+      {% for m in @type.methods %}
+        {% if m.args.length == 0 && m.name.stringify != "keys" %}
+          if key == {{ m.name.stringify }} || key == :{{ m.name.stringify }}
+            return self.{{ m.name }}
+          end
+        {% end %}
+      {% end %}
+
+      {% if @type.superclass && @type.superclass.methods.any?{|m|m.name.stringify == "[]" && m.args.length == 1} %}
+        super
+      {% else %}
+        raise KeyError.new "Missing key value: #{key.inspect}"
+      {% end %}
+    end
+
+    @@%keys = nil
+    def keys
+      if keys = @@%keys
+        keys
+      else
+        keys = {{ @type.methods.select do |m|
+          m.args.length == 0 && m.name.stringify != "keys"
+        end.map{|m|m.name.stringify} }} of String
+
+        {% if @type.methods.any?{|m|m.name.stringify == "keys" && m.args.length == 0} %}
+          keys.concat(previous_def)
+        {% end %}
+        {% if @type.superclass && @type.superclass.methods.any?{|m|m.name.stringify == "keys" && m.args.length == 0} %}
+          keys.concat(super)
+        {% end %}
+        keys.uniq!
+        @@%keys = keys
+        keys
       end
     end
 
-    include {{ "PendantGetter".id }}%mod
-
     macro inherited
       define_pendant_getters
-      {% if @type.methods.any?{|m|m.name.stringify == "__pendant_setter_keys"} %}
-        define_pendant_setters
-      {% end %}
     end
 
     macro included
       define_pendant_getters
-      {% if @type.methods.any?{|m|m.name.stringify == "__pendant_setter_keys"} %}
-        define_pendant_setters
-      {% end %}
     end
   end
 
-  # :nodoc:
   macro included
     define_pendant_getters
   end
--- src/pendant/setter.cr
+++ src/pendant/setter.cr
@@ -5,75 +5,53 @@ module Pendant::Setter
   def keys
   end
 
-  # :nodoc:
   macro define_pendant_setters
-    # :nodoc:
-    module {{ "PendantSetter".id }}%mod
-      macro included
-        def []=(key, value)
-          {% for m in @type.methods %}
-            {% if m.args.length == 1 && m.name.stringify.ends_with?("=") && !m.name.stringify.starts_with?("__") %}
-              if key == {{ (s = m.name.stringify; s[0..-2]) }} || key == :{{ (s = m.name.stringify; s[0..-2]) }}
-                self.{{ m.name }}(value)
-                return value
-              end
-            {% end %}
-          {% end %}
-          {% if @type.superclass && @type.superclass.methods.any?{|m| m.name.stringify == "[]=" && m.args.length == 2} %}
-            super
-          {% else %}
-            # NOTE: `MissingKey` is deprected.  If next version Crystal is released, it renames to `KeyError`.
-            raise MissingKey.new("Missing setter value: #{key.inspect}")
-          {% end %}
-        end
-
-        # :nodoc:
-        def __pendant_setter_keys
-          m = {{ @type.methods.select do |m|
-            m.args.length == 1 && m.name.stringify.ends_with?("=") && m.name.stringify != "keys" && !m.name.stringify.starts_with?("__")
-          end.map{|m| s =m.name.stringify; s[0..-2] }.uniq }} of String
-          {% if @type.superclass && @type.superclass.methods.any?{|m| m.name.stringify == "keys" && m.args.length == 0} %}
-            m.concat(super).uniq
-          {% else %}
-            m
-          {% end %}
-        end
-
-        @@%keys = nil
-        {% if @type.methods.any?{|m|m.name.stringify == "__pendant_getter_keys"} %}
-          def keys
-            return @@%keys.not_nil! if @@%keys
-            @@%keys = self.__pendant_setter_keys.concat(self.__pendant_getter_keys).uniq
-            @@%keys.not_nil!
-          end
-        {% else %}
-          def keys
-            return @@%keys.not_nil! if @@%keys
-            @@%keys = self.__pendant_setter_keys
-            @@%keys.not_nil!
+    def []=(key, value : T)
+      {% for m in @type.methods %}
+        {% if m.args.length == 1 && m.name.stringify.ends_with?("=") && m.name.stringify != "[]=" %}
+          if key == {{ m.name.stringify[0..-2] }} || key == :{{ m.name.stringify[0..-2] }}
+            return self.{{ m.name }}(value)
           end
         {% end %}
+      {% end %}
+
+      {% if @type.superclass && @type.superclass.methods.any?{|m|m.name.stringify == "[]=" && m.args.length == 2} %}
+        super
+      {% else %}
+        raise KeyError.new "Missing key value: #{key.inspect}"
+      {% end %}
+    end
+
+    @@%keys = nil
+    def keys
+      if keys = @@%keys
+        keys
+      else
+        keys = {{ @type.methods.select do |m|
+          m.args.length == 1 && m.name.ends_with?("=") && m.name.stringify != "[]="
+        end.map{|m|m.name.stringify[0..-2]} }} of String
+
+        {% if @type.methods.any?{|m|m.name.stringify == "keys" && m.args.length == 0} %}
+          keys.concat(previous_def)
+        {% end %}
+        {% if @type.superclass && @type.superclass.methods.any?{|m|m.name.stringify == "keys" && m.args.length == 0} %}
+          keys.concat(super)
+        {% end %}
+        keys.uniq!
+        @@%keys = keys
+        keys
       end
     end
 
-    include {{ "PendantSetter".id }}%mod
-
     macro inherited
       define_pendant_setters
-      {% if @type.methods.any?{|m|m.name.stringify == "__pendant_getter_keys"} %}
-        define_pendant_getters
-      {% end %}
     end
 
     macro included
       define_pendant_setters
-      {% if @type.methods.any?{|m|m.name.stringify == "__pendant_getter_keys"} %}
-        define_pendant_getters
-      {% end %}
     end
   end
 
-  # :nodoc:
   macro included
     define_pendant_setters
   end
