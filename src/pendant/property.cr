f7c432b27ee7eaa92a6f1f1ae7aeddab384fd075
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-31 04:54:59 +0900

    Fix setter definition

--- src/pendant/property.cr
+++ src/pendant/property.cr
@@ -4,4 +4,9 @@ require "./setter"
 module Pendant::Property
   include Pendant::Getter
   include Pendant::Setter
+
+  # It returns available keys.
+  # A method of keys have no argument (except `keys`),
+  # or a name of keys ends with '=' and length of its arguments is `1` (except `[]=`).
+  def keys; end
 end
--- src/pendant/setter.cr
+++ src/pendant/setter.cr
@@ -1,14 +1,23 @@
 module Pendant::Setter
-  def []=(key, value)
-  end
+  # Set a `value` calling `key=(value)` method.
+  # If not defined `key=(value)` method, it raise `KeyError`.
+  def []=(key, value); end
 
-  def keys
-  end
+  # It returns available keys.
+  # A name of keys ends with '=' and length of its arguments is `1` (except `[]=`).
+  def keys; end
 
+  # It defines `[]=` and `keys` into your class directory.
+  # Included `Pendant::Setter`, it calls via hooks.
+  #
+  # Defining methods use methods information in the moment,
+  # so please call this macro if you define a new method after including.
   macro define_pendant_setters
+
+    # See `Pendant::Setter#[]=`
     def []=(key, value : T)
       {% for m in @type.methods %}
-        {% if m.args.length == 1 && m.name.stringify.ends_with?("=") && m.name.stringify != "[]=" %}
+        {% if m.args.length == 1 && m.name.stringify.ends_with?("=") %}
           if key == {{ m.name.stringify[0..-2] }} || key == :{{ m.name.stringify[0..-2] }}
             return self.{{ m.name }}(value)
           end
@@ -23,12 +32,14 @@ module Pendant::Setter
     end
 
     @@%keys = nil
+
+    # See `Pendant::Setter#keys`
     def keys
       if keys = @@%keys
         keys
       else
         keys = {{ @type.methods.select do |m|
-          m.args.length == 1 && m.name.ends_with?("=") && m.name.stringify != "[]="
+          m.args.length == 1 && m.name.ends_with?("=")
         end.map{|m|m.name.stringify[0..-2]} }} of String
 
         {% if @type.methods.any?{|m|m.name.stringify == "keys" && m.args.length == 0} %}
